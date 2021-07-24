32b4fa4bf9a42366760cae3e72fb81dd2e886372
Setiel B. Olivarez <setiel_b_olivarez@dlsu.edu.ph>
2021-07-25 02:43:16 +0800

    added broadcasting scripts

---
+++ Assets/Broadcasting/EventNames.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+
+public class EventNames
+{
+    // Start is called before the first frame update
+
+    public const string ON_SPAWN_BUTTON_CLICKED = "ON_SPAWN_BUTTON_CLICKED";
+    public const string ON_CLEAR_BUTTON_CLICKED = "ON_CLEAR_BUTTON_CLICKED";
+}
