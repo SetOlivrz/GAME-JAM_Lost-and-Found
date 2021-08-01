8391f2202ae31fa0ed2f030b3603e376b0285003
Kyle Francis Desquitado <kyle_francis_desquitado@dlsu.edu.ph>
2021-08-01 12:53:45 +0800

    sounds v0.5

--- Assets/Sounds/Scripts/SFXManager.cs
+++ Assets/Sounds/Scripts/SFXManager.cs
@@ -7,33 +7,20 @@ public class SFXManager : MonoBehaviour
     [SerializeField] AudioClip walkingSound;
     [SerializeField] AudioClip runningSound;
     [SerializeField] AudioClip buttonSound;
-<<<<<<< HEAD
-=======
     [SerializeField] AudioClip jumpStartSound;
     [SerializeField] AudioClip jumpEndSound;
     [SerializeField] AudioClip caughtSound;
->>>>>>> parent of 215cc06 (Delete SFXManager.cs)
 
     [SerializeField] private AudioSource audioSourceSFX;
     // Start is called before the first frame update
     void Start()
     {
         audioSourceSFX = this.gameObject.GetComponent<AudioSource>();
-<<<<<<< HEAD
-        EventBroadcaster.Instance.AddObserver(EventNames.ON_PLAYER_WALK_SFX, playWalkingSound);
-        EventBroadcaster.Instance.AddObserver(EventNames.ON_PLAYER_WALK_STOP_SFX, stopWalkingSound);
-
-        EventBroadcaster.Instance.AddObserver(EventNames.ON_PLAY_BUTTON_SFX, playButtonSound);
-
-        EventBroadcaster.Instance.AddObserver(EventNames.ON_PLAYER_CAUGHT, stopWalkingSound);
-        EventBroadcaster.Instance.AddObserver(EventNames.ON_STOP_ALL_SFX, stopAllSounds);
-
-=======
 
         EventBroadcaster.Instance.AddObserver(EventNames.ON_PLAYER_WALK_SFX, playWalkingSound);
         EventBroadcaster.Instance.AddObserver(EventNames.ON_PLAYER_JUMP_START_SFX, playJumpStartSound);
         EventBroadcaster.Instance.AddObserver(EventNames.ON_PLAYER_JUMP_END_SFX, playJumpEndSound);
-        EventBroadcaster.Instance.AddObserver(EventNames.ON_PLAYER_CAUGHT, playCaughtSound);
+        EventBroadcaster.Instance.AddObserver(EventNames.ON_PLAYER_CAUGHT, playCaughtSound  );
 
 
         //ui button clicks
@@ -52,7 +39,6 @@ public class SFXManager : MonoBehaviour
 
 
 
->>>>>>> parent of 215cc06 (Delete SFXManager.cs)
     }
 
     // Update is called once per frame
@@ -66,16 +52,12 @@ public class SFXManager : MonoBehaviour
         EventBroadcaster.Instance.RemoveObserver(EventNames.ON_PLAYER_WALK_SFX);
         EventBroadcaster.Instance.RemoveObserver(EventNames.ON_PLAYER_WALK_STOP_SFX);
 
-<<<<<<< HEAD
-        EventBroadcaster.Instance.RemoveObserver(EventNames.ON_PLAY_BUTTON_SFX);
-=======
         EventBroadcaster.Instance.RemoveObserver(EventNames.ON_OPTIONS_MENU);
         EventBroadcaster.Instance.RemoveObserver(EventNames.ON_PAUSE_GAME);
         EventBroadcaster.Instance.RemoveObserver(EventNames.ON_RESUME_GAME);
         EventBroadcaster.Instance.RemoveObserver(EventNames.ON_QUIT_TO_MENU);
         EventBroadcaster.Instance.RemoveObserver(EventNames.ON_QUIT_GAME);
         EventBroadcaster.Instance.RemoveObserver(EventNames.ON_START_GAME);
->>>>>>> parent of 215cc06 (Delete SFXManager.cs)
 
     }
     private void playWalkingSound(Parameters parameters)
@@ -89,23 +71,14 @@ public class SFXManager : MonoBehaviour
             audioSourceSFX.clip = runningSound;
         if (!audioSourceSFX.isPlaying)
             audioSourceSFX.Play();
-<<<<<<< HEAD
-=======
 
         //Debug.Log("play walking sound");
->>>>>>> parent of 215cc06 (Delete SFXManager.cs)
     }
 
     private void stopWalkingSound()
     {
         if (audioSourceSFX != null)
         {
-<<<<<<< HEAD
-            if (audioSourceSFX.clip == walkingSound || audioSourceSFX.clip == runningSound)
-                audioSourceSFX.Stop();
-        }
-        
-=======
             if ((audioSourceSFX.clip == walkingSound || audioSourceSFX.clip == runningSound) && audioSourceSFX.isPlaying == true)
                 audioSourceSFX.Stop();
         }
@@ -115,31 +88,27 @@ public class SFXManager : MonoBehaviour
 
     private void playJumpStartSound()
     {
-        //audioSourceSFX.clip = jumpStartSound;
-        AudioSource.PlayClipAtPoint(jumpStartSound, transform.position, audioSourceSFX.volume);
-        
-        {
-            Debug.Log("playing jump start sound");
-        }
+        if (jumpStartSound != null)
+            AudioSource.PlayClipAtPoint(jumpStartSound, transform.position, audioSourceSFX.volume);
+
+        else Debug.Log("no jumpstart sound file attached");
     }
 
     private void playJumpEndSound()
     {
-        AudioSource.PlayClipAtPoint(jumpEndSound, transform.position, audioSourceSFX.volume);
-        {
-            Debug.Log("playing jump end sound");
-        }
+        if (jumpEndSound != null)
+            AudioSource.PlayClipAtPoint(jumpEndSound, transform.position, audioSourceSFX.volume);
+
+        else Debug.Log("no jumpend sound file attached");
     }
 
     private void playCaughtSound()
     {
         //audioSourceSFX.clip = jumpStartSound;
-        AudioSource.PlayClipAtPoint(caughtSound, transform.position, audioSourceSFX.volume);
+        if (caughtSound != null)
+            AudioSource.PlayClipAtPoint(caughtSound, transform.position, audioSourceSFX.volume);
 
-        {
-            Debug.Log("playing caught sound");
-        }
->>>>>>> parent of 215cc06 (Delete SFXManager.cs)
+        else Debug.Log("no caught sound file attached");
     }
 
     private void playButtonSound()
