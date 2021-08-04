134234765152eb932ce4f25b7eff9b9bfd5eef4a
denise_taiza <maria_alyssa_taiza@dlsu.edu.ph>
2021-08-05 00:07:34 +0800

    Added bookmark

--- Assets/UI/Scripts/DirectionalArrow.cs
+++ Assets/UI/Scripts/DirectionalArrow.cs
@@ -6,20 +6,25 @@ public class DirectionalArrow : MonoBehaviour
 {
 
     [SerializeField] private Transform targetKey;
-
+    [SerializeField] private GameObject bookMark;
 
 
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        
+        bookMark.transform.position = new Vector3(this.transform.position.x, this.transform.position.y - 0.3f, this.transform.position.z);
     }
 
     // Update is called once per frame
     void Update()
     {
-        //Vector3 targetPosition = targetKey.transform.position;
-        //targetPosition.y = this.transform.position.y;
-        //transform.LookAt(targetPosition);
+        Vector3 targetPosition = targetKey.transform.position;
+        targetPosition.y = this.transform.position.y;
+        transform.LookAt(targetPosition);
+
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            bookMark.transform.position = new Vector3(this.transform.position.x, this.transform.position.y - 0.3f, this.transform.position.z);
+        }
     }
 }
